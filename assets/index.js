c7fec5b0eb12394db538671efd5e1d129daf172f
Ian M <ianmillichamp.2@gmail.com>
2023-01-09 11:32:16 +0000

    add js code to display the randomly generated password in the input box

--- assets/index.js
+++ assets/index.js
@@ -1,7 +1,7 @@
 //create Javascript to enable password generator to work
 //create a variable that connects to the inputs id - password
 let password = document.getElementById("password");
-console.log(password)
+
 //create a function for generating the password
 
 function generatePassword() {
@@ -22,10 +22,12 @@ let password = "";
     2) set index to be less than or equal to the variable password length to ensure the generated password has no more than 12 characters
     3) i ++ means that the loop will continue to go from 1 - 12 to grab a symbol as the statement i <=passwordLength is true (once the expression becomes false by reaching 13 or above the loop will cease to funbction.)
     4) make a variable - random characters that is equal to the Math.floor function which is composed of the math.random function picking any character from along the length of the characters variable string element.
-    5) finish the for loop by saying that password 
+    5) finish the for loop by using the += operator to add the value of characters substring to the password variable
 */
 for (let i = 0; i < passwordLength.length; i++) {
     let  randomCharacter = Math.floor(Math.random() * characters.length);
     password += characters.substring(randomCharacter,randomCharacter +1);
     
 }
+// assign the password to the input box . This will allow the pass word to be seen in it.
+document.getElementById("password").value = password
