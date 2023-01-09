5f0ce6e53021868843e6af3b4aee49876db39f5f
Ian M <ianmillichamp.2@gmail.com>
2023-01-09 11:09:55 +0000

    created a functiin in the js that contains a variable for the possible characters for the password, a variable defining the length of the password and a variable for the generated password to be logged in

--- assets/index.js
+++ assets/index.js
@@ -2,7 +2,17 @@
 //create a variable that connects to the inputs id - password
 let password = document.getElementById("password");
 console.log(password)
-//create a variable that contains all letters/special symbols/ numbers that the random password can be made from
-let characters = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!£%^&*()_+-={}[]:@~#<>?,./|";
+//create a function for generating the password
+
+function generatePassword() {
+ //create a variable that contains all letters/special symbols/ numbers that the random password can be made from   
+    let characters = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!£%^&*()_+-={}[]:@~#<>?,./|";
 console.log(characters)
 // note had to remove some symbols as they have effect on javascript such as $;'.
+// we have been asked to give the password a length of 12 characters therefore i will create a variable equal to 12.
+let passwordLength = 12;
+console.log(passwordLength)
+
+//define a variable for password in the function to equal the symbols from the string in the variable characters
+let password = "";
+}
