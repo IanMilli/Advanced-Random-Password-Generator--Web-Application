7f1da49ac257f596f241f6d6d83353c1e6ad6216
Ian M <ianmillichamp.2@gmail.com>
2023-01-09 11:26:01 +0000

    added a for loop that will iterate through the characters variable and create a substring with a limit of 12 randomly picked characters

--- assets/index.js
+++ assets/index.js
@@ -16,3 +16,16 @@ console.log(passwordLength)
 //define a variable for password in the function to equal the symbols from the string in the variable characters
 let password = "";
 }
+
+// create the random pass word using a for loop and the Math.random function
+/*  1) change all uses of the word index to i
+    2) set index to be less than or equal to the variable password length to ensure the generated password has no more than 12 characters
+    3) i ++ means that the loop will continue to go from 1 - 12 to grab a symbol as the statement i <=passwordLength is true (once the expression becomes false by reaching 13 or above the loop will cease to funbction.)
+    4) make a variable - random characters that is equal to the Math.floor function which is composed of the math.random function picking any character from along the length of the characters variable string element.
+    5) finish the for loop by saying that password 
+*/
+for (let i = 0; i < passwordLength.length; i++) {
+    let  randomCharacter = Math.floor(Math.random() * characters.length);
+    password += characters.substring(randomCharacter,randomCharacter +1);
+    
+}
