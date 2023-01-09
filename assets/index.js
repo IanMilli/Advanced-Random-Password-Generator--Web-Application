f46773f6cf899be1e9b3af67442afbc184b06ae4
Ian M <ianmillichamp.2@gmail.com>
2023-01-09 12:35:46 +0000

    used console log to id issue preventing password function to either generate random password or display appropiately in the input box

--- assets/index.js
+++ assets/index.js
@@ -1,21 +1,21 @@
 //create Javascript to enable password generator to work
 //create a variable that connects to the inputs id - password
-let password = document.getElementById("password");
-console.log(password)
+let password=document.getElementById("password");
+console.log(password);
 //create a function for generating the password
 
 function generatePassword() {
  //create a variable that contains all letters/special symbols/ numbers that the random password can be made from   
     let characters = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!£%^&*()_+-={}[]:@~#<>?,./|";
-console.log(characters)
+console.log(characters);
 // note had to remove some symbols as they have effect on javascript such as $;'.
 // we have been asked to give the password a length of 12 characters therefore i will create a variable equal to 12.
 let passwordLength = 12;
-console.log(passwordLength)
+console.log(passwordLength);
 
 //define a variable for password in the function to equal the symbols from the string in the variable characters
 let password = "";
-
+console.log(password);
 
 // create the random pass word using a for loop and the Math.random function
 /*  1) change all uses of the word index to i
@@ -24,7 +24,7 @@ let password = "";
     4) make a variable - random characters that is equal to the Math.floor function which is composed of the math.random function picking any character from along the length of the characters variable string element.
     5) finish the for loop by using the += operator to add the value of characters substring to the password variable
 */
-for (let i = 0; i <= passwordLength.length; i++) {
+for (let i = 0; i <= passwordLength; i++) {
     let  randomCharacter = Math.floor(Math.random() * characters.length);
     password += characters.substring(randomCharacter,randomCharacter +1);
     
@@ -33,11 +33,11 @@ for (let i = 0; i <= passwordLength.length; i++) {
         document.getElementById("password").value = password;
 }
 // lastly create a function to allow the copy button to copy the random generated password on to the clipboard for the user.
-function Copy () {
+function CopyPassword () {
     // create a variable to be equal to  the value of the id (password)
-    let copyPassword = document.getElementById("password");
+    let copyPW = document.getElementById("password");
    // use the select option to grab the value of the copyPassword variable
-    copyPassword.select();
+    copyPW.select();
     // then use the method execCommand to copy the value
 document.execCommand("copy");
 }
